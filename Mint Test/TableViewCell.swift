868041a3c563d0685af7d40b5861874c14d53876
Maxwell Obinna <maxsteelobinna@gmail.com>
2020-07-28 14:13:04 +0100

    [max] Officially done with the task

--- Mint Test/TableViewCell.swift
+++ Mint Test/TableViewCell.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
+    @IBOutlet weak var number: UILabel!
+    @IBOutlet weak var message: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Mint Test/TableViewController.swift
+++ Mint Test/TableViewController.swift
@@ -12,8 +12,6 @@ import RealmSwift
 class TableViewController: UITableViewController {
 
     @IBOutlet weak var spinner: UIActivityIndicatorView!
-    
-    
     var gitArray : Results<Commit>?
     
     lazy var refresh: UIRefreshControl = {
@@ -24,34 +22,72 @@ class TableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        spinner?.startAnimating()
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        GitCaller.getCommit(){
-            responseModel in
-            if responseModel.success{
-                
-            }else{
-                self.presentOkAlert(responseModel.title ?? "Error", responseModel.generalMessage ?? "")
-            }
-            self.spinner?.stopAnimating()
-            self.spinner?.isHidden = true
+     
+        self.tableView.estimatedRowHeight = 200
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        if #available(iOS 10.0, *) {
+            self.tableView.refreshControl = refresh
+        } else {
+            self.tableView.addSubview(refresh)
         }
+        
+        do{
+            let realm = try Realm()
+            gitArray = realm.objects(Commit.self)
+        }catch{}
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let inset = UIEdgeInsets(top: 0, left: 0, bottom: 100, right: 0)
+        self.tableView.contentInset = inset
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tableView.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if let size = gitArray?.count {
+            if size < 1 {
+                loadData()
+            }
+        }else{
+            loadData()
+        }
+    }
+    
+    func loadData() {
+        spinner?.startAnimating()
+        
+         GitCaller.getCommit(){
+                   responseModel in
+                   if responseModel.success{
+                       
+                   }else{
+                       self.presentOkAlert(responseModel.title ?? "Error", responseModel.generalMessage ?? "")
+                   }
+                   self.spinner?.stopAnimating()
+                   self.spinner?.isHidden = true
+               }
+               
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return gitArray?.count ?? 0
     }
     
     @objc private func handleRefresh(_ refreshControl: UIRefreshControl) {
@@ -69,59 +105,16 @@ class TableViewController: UITableViewController {
         
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let git = gitArray?[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath) as! TableViewCell
+        //cell.number.text = String(indexPath.row + 1)
+        cell.number.isHidden = true
+        cell.message.text = git?.commit?.message
         // Configure the cell...
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
