75c061983932fa5bb503344075ea0a79889dfcb1
Maxwell Obinna <maxsteelobinna@gmail.com>
2020-07-28 12:31:29 +0100

    [max] Created Objects for mappable with realm and Object Mapper

---
+++ Mint Test/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Mint Test
+//
+//  Created by Maxwell Nwanna on 28/07/2020.
+//  Copyright © 2020 Maxwell. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
